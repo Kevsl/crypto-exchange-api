77f9f59251c766d993d5bab4035961d88662c135
Kevsl <ktr2@pm.me>
2024-06-14 15:35:01 +0200

    added token sell to bank

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+async function main() {
+  await this.prisma.role.create({
+    name: 'user',
+  });
+  await this.prisma.role.create({
+    name: 'admin',
+  });
+
+  await this.prisma.promoCode.create({
+    name: 'PROMO1000',
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
